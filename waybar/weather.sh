232900195f10a68ec70e32799b6bd0a9ee52cd9e
Thoq <tristanshaw@gmail.com>
2025-08-21 13:44:00 -0400

    fixed weather

--- waybar/weather.sh
+++ waybar/weather.sh
@@ -1,19 +1,16 @@
-#!/usr/bin/sh
+#!/bin/sh
 
 # Set to true for Celsius, false for Fahrenheit
 metric=false
 
-req=$(curl -s wttr.in/CITY?format="%t|%l+(%c%f)+%h,+%C")
-bar=$(echo $req | awk -F "|" '{print $1}')
-tooltip=$(echo $req | awk -F "|" '{print $2}')
-temp_celsius=$(echo $bar | sed 's/[^0-9.-]*\([0-9.-]*\)[^0-9.-]*/\1/')
-temp_fahrenheit=$(( (temp_celsius * 9 / 5) + 32 ))
-temp_fahrenheit=$(printf "%.0f" $temp_fahrenheit)
-
 if [ "$metric" = true ]; then
-    bar="$temp_celsius°C"
+  req=$(curl -s "wttr.in/CITY?format=%t|%l+(%c%f)+%h,+%C&m")
+  bar=$(echo $req | awk -F "|" '{print $1}' | sed 's/^+//')
+  tooltip=$(echo $req | awk -F "|" '{print $2}')
+  echo "{\"text\":\"$bar\", \"tooltip\":\"$tooltip\"}"
 else
-    bar="$temp_fahrenheit°F"
+  req=$(curl -s "wttr.in/CITY?format=%t|%l+(%c%f)+%h,+%C")
+  bar=$(echo $req | awk -F "|" '{print $1}' | sed 's/^+//')
+  tooltip=$(echo $req | awk -F "|" '{print $2}')
+  echo "{\"text\":\"$bar\", \"tooltip\":\"$tooltip\"}"
 fi
-
-echo "{\"text\":\"$bar\", \"tooltip\":\"$tooltip\"}"
